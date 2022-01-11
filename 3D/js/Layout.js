eab7b2f7cba1165ca69dc100adbb8e3e524411e6
Angel CC <angelcenerini@gmail.com>
2022-01-10 19:12:32 -0500

    PW: Updated Page Layout

--- 3D/js/Layout.js
+++ 3D/js/Layout.js
@@ -64,14 +64,27 @@ class Layout{
     this.growth2 = 1;
     this.growth3 = 1;
     // Descriptions
-    this.expanded = false;
+    this.expanded1 = false;
+    this.descriptionX1 = x1;
+    this.descriptionY1 =  y1;
+    this.expanded2 = false;
+    this.descriptionX2 = x2;
+    this.descriptionY2 =  y2;
+    this.expanded3 = false;
+    this.descriptionX3 = x3;
+    this.descriptionY3 =  y3;
+    this.descriptionOpacity = 200;
+    this.transparencyOpacity = 10;
     this.description = undefined;
     this.description1 = `
-    Feathered humanoid character. Mythological creature interpretating
-    the unfamous behaviour of the cuckoo birds species.`;
+    Feathered humanoid character.
+    Mythological creature interpretating
+    the unfamous behaviour of
+    the cuckoo bird species.`;
     this.description2 = `
-    Reverse concept of the window cleaner career, littered with domestic scenes.
-    Following a celestial palette and feauturing small twists.`;
+    Reverse concept of the window
+    cleaner career, littered
+    with domestic (twisted) scenes.`;
     this.description3 = `
     Biomechanical interpretation of hammerhead shark built
     via hardsurface tools and components.`;
@@ -80,45 +93,9 @@ class Layout{
   }
 
   update(){
-    // this.setState();
     this.addInfo();
     this.display();
   }
-
-  // setState(){
-  //   // Check State
-  //   // Images & Text
-  //   if (state === `layout`){
-  //     // Play Button
-  //     this.interactive = true;
-  //     // Images
-  //     this.image1 = kitbash2;
-  //     this.image2 = kitbash2;
-  //     this.image3 = kitbash2;
-  //     // Text
-  //     this.name1 = `Kitbash1`;
-  //     this.name2 = `Kitbash2`;
-  //     this.name3 = `Kitbash3`;
-  //     this.text = `PLAY`;
-  //     this.details1 = `
-  //     Unity
-  //     Texture Painter
-  //     Cacca`;
-  //     this.details2 = `
-  //     Blender
-  //     UV Coloring`;
-  //     this.details3 = `
-  //     Blender
-  //     Substance 3D Painter`;
-  //     this.description1 = `
-  //     Dynamic and easy to navigate, CP3's new website embodies the passionate
-  //     commitment of its members by thoroughly showcasing its mission and projects.`;
-  //     this.description2 = `
-  //     Conventional website layout featuring game mechanincs.`;
-  //     this.description3 = `
-  //     Portfolio Website. Hope you're enjoying it.`;
-  //   }
-  // }
 
   addInfo(){
     // Hover over Plus Button
@@ -133,11 +110,10 @@ class Layout{
           this.growth1 = 0;
         }
         // Display Description
-        this.expanded = true;
-        this.description = this.description1;
+        this.expanded1 = true;
       }
       else{
-        this.expanded = false;
+        this.expanded1 = false;
         this.plusButtonSize1 = 45;
         this.growth1 = 1;
       }
@@ -149,11 +125,10 @@ class Layout{
           this.growth2 = 0;
         }
         // Display Description
-        this.expanded = true;
-        this.description = this.description2;
+        this.expanded2 = true;
       }
       else{
-        this.expanded = false;
+        this.expanded2 = false;
         this.plusButtonSize2 = 45;
         this.growth2 = 1;
       }
@@ -167,11 +142,10 @@ class Layout{
           this.growth3 = 0;
         }
         // Display Description
-        this.expanded = true;
-        this.description = this.description3;
+        this.expanded3 = true;
       }
       else{
-        this.expanded = false;
+        this.expanded3 = false;
         this.plusButtonSize3 = 45;
         this.growth3 = 1;
       }
@@ -183,15 +157,15 @@ class Layout{
 
     push();
 
-    if(this.expanded){
-      // Description
-      textFont(`Courier`);
-      textAlign(CENTER);
-      textSize(18);
-      let descriptionX = width/2;
-      let descriptionY = 4*height/5;
-      text(this.description, descriptionX, descriptionY);
-    }
+      // // Description
+      // textFont(`Courier`);
+      // textAlign(CENTER);
+      // textSize(18);
+      // let descriptionX = width/2;
+      // let descriptionY = 4*height/5;
+      // text(this.description, descriptionX, descriptionY);
+
+    // }
 
     // Third Project
     if(!this.firstPage){
@@ -212,12 +186,25 @@ class Layout{
       textSize(16);
       text(this.details3, this.x3 + 2*this.imageWidth/3 - 100, this.y3 - 100);
 
+      // Description 3
+      if (this.expanded3){
+        // Text Box
+        fill(42, this.descriptionOpacity);
+        rect(this.descriptionX3, this.descriptionY3, this.imageWidth, this.imageHeight);
+        // Text
+        fill(254, 253, 249);
+        textAlign(LEFT);
+        textSize(19);
+        text(this.description3, this.descriptionX3 - this.imageWidth/2, this.descriptionY3);
+      }
+
       // PLus Button
       fill(253, 221, 107);
       ellipse(this.plusButtonX3, this.plusButtonY3, this.plusButtonSize3);
       // PLus Button Text
       fill(42);
-      textAlign(CENTER);
+      textAlign(CENTER);false
+      textSize(20);
       text(`+`, this.plusButtonX3, this.plusButtonY3 - 2);
 
       // Check if State features Play Button
@@ -270,12 +257,37 @@ class Layout{
         text(this.text, this.buttonX2, this.buttonY2);
       }
 
+      // Description 1
+      if (this.expanded1){
+        // Text Box
+        fill(42, this.descriptionOpacity);
+        rect(this.descriptionX1, this.descriptionY1, this.imageWidth, this.imageHeight);
+        // Text
+        fill(254, 253, 249);
+        textAlign(LEFT);
+        textSize(19);
+        text(this.description1, this.descriptionX1 - this.imageWidth/2, this.descriptionY1);
+      }
+      if(this.expanded2){
+        // Description 2
+        // Text Box
+        fill(42, this.descriptionOpacity);
+        rect(this.descriptionX2, this.descriptionY2, this.imageWidth, this.imageHeight);
+        // Text
+        fill(254, 253, 249);
+        textAlign(LEFT);
+        textSize(19);
+        text(this.description2, this.descriptionX2 - this.imageWidth/2, this.descriptionY2);
+      }
+
       // PLus Button
       fill(253, 221, 107);
       ellipse(this.plusButtonX1, this.plusButtonY1, this.plusButtonSize1);
       ellipse(this.plusButtonX2, this.plusButtonY2, this.plusButtonSize2);
       // PLus Button Text
       fill(42);
+      textAlign(CENTER);
+      textSize(20);
       text(`+`, this.plusButtonX1, this.plusButtonY1 - 2);
       text(`+`, this.plusButtonX2, this.plusButtonY2 - 2);
     }
